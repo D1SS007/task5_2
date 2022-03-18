991748906da7f9b59fd393433adc5b8906f51191
Alex Zverev <lordzver007@yandex.ru>
2022-03-18 21:12:18 +0300

    Добавьте файлы проекта.

---
+++ task5_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace task5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Иван";
+            string secondName = "Иванов";
+            string tempReplaceSpace;
+
+            Console.WriteLine(firstName + " " + secondName );
+
+            tempReplaceSpace = firstName;
+            firstName = secondName;
+            secondName = tempReplaceSpace;            
+
+            Console.WriteLine(firstName + " " +secondName);
+        }
+    }
+}
